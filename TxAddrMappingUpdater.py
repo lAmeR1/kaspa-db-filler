77d017f394b1e81d30d5608cec5a19ca07577d9d
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-04-02 21:23:49 +0200

    * added TxAddrMapping Table * check if last block time is changing, if not sleep as well and give info
    Signed-off-by: lAmeR <42315864+lAmeR1@users.noreply.github.com>

--- TxAddrMappingUpdater.py
+++ TxAddrMappingUpdater.py
@@ -5,19 +5,20 @@ import time
 from datetime import datetime, timedelta
 
 from dbsession import session_maker
+from models.TxAddrMapping import TxAddrMapping
 
 LIMIT = 2000
+PRECONDITION_RETRIES = 2
 
 _logger = logging.getLogger(__name__)
 
 
 class TxAddrMappingUpdater(object):
     def __init__(self):
-        for i in range(2):
-            if i > 0:
-                _logger.info(f"Retry {i}. Wait 10s")
-                time.sleep(10)
+        self.last_block_time = None
 
+    def precondition(self):
+        for i in range(PRECONDITION_RETRIES):
             with session_maker() as s:
                 res = s.execute("SELECT block_time "
                                 "FROM tx_id_address_mapping "
@@ -25,22 +26,30 @@ class TxAddrMappingUpdater(object):
                                 "DESC "
                                 "LIMIT 1").first()
 
-            # get last added block time and substract 1 hour just for instance
-            try:
-                self.last_block_time = res[0] - 1000 * 60 * 60
-                break
-            except:
-                _logger.info('No block time found in database.')
+                # get last added block time and substract 1 hour just for instance
+                if res:
+                    self.last_block_time = res[0] - 1000 * 60 * 60
+                    break
+                else:
+                    _logger.info('No block time found in database. Fetching from blocks')
+                    res = s.execute("SELECT timestamp FROM blocks ORDER by blue_score ASC LIMIT 1").first()
+                    if res:
+                        self.last_block_time = int(res[0].timestamp() * 1000)
+                        break
 
+            _logger.info(f"Retry {i + 1}. Wait 10s")
+            time.sleep(10)
 
     @staticmethod
     def minimum_timestamp():
         return round((datetime.now() - timedelta(minutes=1)).timestamp() * 1000)
 
     def loop(self):
+        self.precondition()
+
         error_cnt = 0
 
-        _logger.debug('Start TxAddrMappingUpdater')
+        _logger.debug('Start TxAddrMappingUpdater')  # type: TxAddrMapping
 
         while True:
             try:
@@ -71,15 +80,21 @@ class TxAddrMappingUpdater(object):
             min_timestamp = TxAddrMappingUpdater.minimum_timestamp()
 
             # new last block times are sorted in the database!
-            self.last_block_time = min(new_last_block_time_outputs or new_last_block_time,
+            next_block_time = min(new_last_block_time_outputs or new_last_block_time,
                                        min_timestamp,
                                        new_last_block_time_inputs or new_last_block_time)
+
+            if self.last_block_time == next_block_time:
+                # this only happens, when kaspad is not synced and the last block ( far in the past ) is not changing
+                _logger.info('DB is not filled with the current blocks. Wait.')
+                time.sleep(10)
+
+            self.last_block_time = next_block_time
 
             _logger.debug(f"Added TxAddrMapping up to: "
                           f"{datetime.fromtimestamp(self.last_block_time / 1000).isoformat()}")
 
             if self.last_block_time == min_timestamp:
-                # updater catched up. You have time
                 time.sleep(10)
 
     def get_last_block_time(self, start_block_time):
---
+++ models/TxAddrMapping.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, BigInteger, Boolean, UniqueConstraint
+
+from dbsession import Base
+
+
+class TxAddrMapping(Base):
+    __tablename__ = 'tx_id_address_mapping'
+    transaction_id = Column(String)
+    address = Column(String)
+    block_time = Column(BigInteger)
+    is_accepted = Column(Boolean, default=False)
+    id = Column(BigInteger, primary_key=True)
+
+    __table_args__ = (UniqueConstraint('transaction_id', 'address',
+                                       name='tx_id_address_mapping_transaction_id_address_key'),)
